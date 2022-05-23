bf6e9bccd88694ba1fd849951a134ccbd7c6f9f2
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-23 12:40:09 -0300

    criacao module e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JogoModule } from './jogos/jogo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [JogoModule],
+  imports: [JogoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
