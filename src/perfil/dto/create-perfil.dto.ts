e0d260b58aca135c0c967353aef58991d0ed83f3
violigon <vinicius@oligon.com.br>
2022-05-27 00:20:33 -0300

    feat(dto): include jogo and userId

--- src/perfil/dto/create-perfil.dto.ts
+++ src/perfil/dto/create-perfil.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUrl, Matches, MinLength } from 'class-validator';
 
 export class CreatePerfilDto {
-
   @IsString()
   @ApiProperty({
     description: 'Nome de perfil. Utilizado no login. Deve ser único',
@@ -13,9 +12,10 @@ export class CreatePerfilDto {
   @IsUrl()
   @ApiProperty({
     description: 'Imagem de perfil do usuário',
-    example: 'https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8=',
+    example:
+      'https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8=',
   })
   ImageURL: string;
-
-
+  jogoIds: string[];
+  userId: string;
 }
