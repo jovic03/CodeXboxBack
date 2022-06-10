af6a88b9ccb94df7900b6042513b4afceba6d928
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-06-10 19:12:58 -0300

    Update favoritos.service.ts

--- src/favoritos/favoritos.service.ts
+++ src/favoritos/favoritos.service.ts
@@ -1,24 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFavoritoDto } from './dto/create-favorito.dto';
 import { UpdateFavoritoDto } from './dto/update-favorito.dto';
 import { handleError } from 'src/utils/handle-error.util';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Favorito } from './entities/favorito.entity';
 
 @Injectable()
 export class FavoritosService {
-  create(createFavoritoDto: CreateFavoritoDto) {
-    return 'This action adds a new favorito';
+
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createFavoritoDto: CreateFavoritoDto) {
+
+
+
+    const favorito = await this.prisma.jogosFavoritos.findUnique({
+      where:{
+        perfilId_jogoId: {...createFavoritoDto}
+      }
+    })
+
+    if(favorito){
+      throw new NotFoundException('Você já favoritou este jogo.')
+    }
+
+    return this.prisma.jogosFavoritos.create({
+      data:{
+        jogoId:createFavoritoDto.jogoId,
+        perfilId:createFavoritoDto.perfilId
+      }
+    })
   }
 
-  findAll() {
-    return `This action returns all favoritos`;
+  findAll(): Promise<Favorito[]> {
+    return this.prisma.jogosFavoritos.findMany() ;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} favorito`;
+  async findOne(id: string) {
+    const record = await this.prisma.jogosFavoritos.findUnique({
+      where:{id}
+    });
+
+    if (!record){
+      throw new NotFoundException(`Registro com o '${id}' não encontrado.`)
+    }
+
+    return record;
   }
 
 
-  remove(id: string) {
-    return `This action removes a #${id} favorito`;
+  async delete(id: string) {
+
+    await this.findOne(id);
+
+    await this.prisma.jogosFavoritos.delete({where:{id}})
   }
 }
